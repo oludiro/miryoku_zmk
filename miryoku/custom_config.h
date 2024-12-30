6753823131c8ab8c2386fedc30109141e746a941
Oktavianus Ludiro <oludiro@users.noreply.github.com>
2024-12-30 13:30:45 -0800

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,4 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_COLEMAKDH
+#define MIRYOKU_EXTRA_QWERTY
+#define MIRYOKU_NAV_VI
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
